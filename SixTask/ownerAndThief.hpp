15cc75736de57db23a39669fbdcb3b3c7f7219cc
Andrey <belyakov.andrey.n@gmail.com>
2021-12-12 17:24:45 +0300

    Complete 6.3

--- SixTask/ownerAndThief.hpp
+++ SixTask/ownerAndThief.hpp
@@ -8,20 +8,73 @@
 #include <ctime>
 #include <algorithm>
 
-std::vector<int> things;
+
+class Things
+{
+private:
+    std::vector<int> things = {88,55,21,7,14,56,41,74,58,65};
+    std::mutex m;
+
+public:
+    Things()
+    {
+
+    }
+    void addEl(int el)
+    {
+        std::lock_guard<std::mutex> lock(m);
+        things.push_back(el);
+    }
+
+    void stoleEl()
+    {
+        things.erase(std::max_element(things.begin(), things.end()));
+    }
+
+    void printThings()
+    {
+        std::lock_guard<std::mutex> lock(m);
+        for (const auto& t : things)
+        {
+            std::cout << t << " ";
+        }
+    }
+
+    bool isEmlpty() const
+    {
+        return things.empty();
+    }
+};
+
+Things things;
 
 void owner()
 {
+    while(true)
+    {
+        if (things.isEmlpty())
+            break;
+        things.printThings();
+        things.addEl(rand()%100+1);
+        
+        std::cout << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+        
+    }
     
-    things.push_back(rand()%100+1);
-    std::cout << "Owner add a: " << *(things.end()-1) << std::endl;
 }
 
 void thief()
 {
-    auto del = std::max_element(things.begin(), things.end());
-    std::cout << "Thief stole a: " << *del << std::endl;
-    things.erase(del);
+    while(true)
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        things.stoleEl();
+        if (things.isEmlpty())
+            break;
+        
+    }
+    std::cout << "Thief stoled all things!" << std::endl;
     
 }
 
